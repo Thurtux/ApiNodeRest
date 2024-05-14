a21e5b41c3d4c5d19ac7c6d3f5d1e79bd6a67fc3
Thurtux <arthursantoslopes91@gmail.com>
2024-05-14 01:08:18 +0100

    Creating Users model

--- src/database/Schemas/User.ts
+++ src/database/Schemas/User.ts
@@ -1,4 +1,26 @@
 import mongoose from "mongoose";
 
 const User = new mongoose.Schema({
-})
+    name: {
+        type: String,
+        require: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true
+        //lowerCase: tipo conversao minuscula
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export default mongoose.model("User", User);
