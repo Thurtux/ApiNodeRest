980ffc40590f6406851b7846afa7590e1fcb4106
Thurtux <arthursantoslopes91@gmail.com>
2024-05-14 02:35:10 +0100

    creating search users

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,6 +2,21 @@ import { Request, Response } from "express";
 import User from "../database/Schemas/User";
 
 class UseController {
+
+    async find(request: Request, response:Response) {
+    try {
+        const users = await User.find();
+        return response.json(users);
+    }
+    catch (error) {
+        return response.status(500).send({
+            error: "search not performed",
+            message: error
+        })
+     }
+
+    }
+
     async create(request: Request, response:Response) {
             const {name, email, password} = request.body;
         try {
