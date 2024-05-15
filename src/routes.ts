88a257ff19410610c327eace0c64a0a4fa74fe8d
Thurtux <arthursantoslopes91@gmail.com>
2024-05-15 02:45:28 +0100

    correcting users route

--- src/routes.ts
+++ src/routes.ts
@@ -3,7 +3,7 @@ import UserController from "./controllers/UserController"
 
 const routes = Router();
 
-routes.post("/users", UserController.find)
+routes.get("/users", UserController.find)
 routes.post("/user", UserController.create);
 
 export default routes;
