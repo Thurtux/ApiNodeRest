a8f005b7ee84d67b7a38f5137d010a4a4023e537
Thurtux <arthursantoslopes91@gmail.com>
2024-05-20 20:56:23 -0300

    Car routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,22 @@
 import { Router } from "express";
 import UserController from "./controllers/UserController"
+import CarController from "./controllers/CarController";
 
 const routes = Router();
 
+//users routes
 routes.delete("/user", UserController.delete);
 routes.get("/users", UserController.find);
 routes.post("/user", UserController.create);
 
+
+
+//cars routes
+routes.delete("/car", CarController.delete);
+routes.get("/cars", CarController.find);
+routes.post("/car", CarController.create);
+routes.put("/car", CarController.update)
 export default routes;
+
+
+
