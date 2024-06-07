14778ea54359c85a32e485ea71cdd5136511efba
Thurtux <arthursantoslopes91@gmail.com>
2024-06-07 21:45:35 +0100

    Add Cors

--- src/routes.ts
+++ src/routes.ts
@@ -2,12 +2,17 @@ import { Router } from "express";
 import UserController from "./controllers/UserController"
 import CarController from "./controllers/CarController";
 import router from "./auth/UserRegister";
+import cors from 'cors';
+
 
 const routes = Router();
+routes.use(cors());
 
 //users routes
-routes.delete("/user", UserController.delete);
 routes.get("/users", UserController.find);
+
+
+routes.delete("/user", UserController.delete);
 routes.post("/user", UserController.create);
 
 
