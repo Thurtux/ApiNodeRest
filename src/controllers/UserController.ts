4867ad97559640a3ed9ffdeb382aeeb5846ea231
Thurtux <arthursantoslopes91@gmail.com>
2024-06-10 09:25:21 -0300

    Creating login in UserController

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,79 +1,122 @@
 import { Request, Response } from "express";
 import User from "../database/Schemas/User";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
 
-class UseController {
+dotenv.config();
 
-    async find(request: Request, response:Response) {
-    try {
-        const users = await User.find();
-        return response.json(users);
+class UserController {
+    async find(request: Request, response: Response) {
+        try {
+            const users = await User.find();
+            return response.json(users);
+        } catch (error: any) {
+            return response.status(500).send({
+                error: "search not performed",
+                message: error.message // Use error.message para mostrar a mensagem de erro
+            });
+        }
     }
-    
-    catch (error) {
-        return response.status(500).send({
-            error: "search not performed",
-            message: error
-        })
-     }
 
-    }
+    async create(request: Request, response: Response) {
+        const { name, email, password } = request.body;
 
-    async create(request: Request, response:Response) {
-            const {name, email, password} = request.body;
         try {
             const userExists = await User.findOne({ email });
 
-            if(userExists) {
+            if (userExists) {
                 return response.status(400).json({
                     error: "oops",
                     message: "user already exist",
                 });
             }
 
+            const hashedPassword = await bcrypt.hash(password, 10); // Hash da senha
+
             const user = await User.create({
                 name,
                 email,
-                password
+                password: hashedPassword, // Salvar a senha hashada
             });
+
             return response.json(user);
 
-    
-        }
-         catch (error) {
+        } catch (error: any) {
             return response.status(500).send({
-                error: "Registration failed",
-                message: error
-            })
-         }
+                error: "Nao foi possivel criar usuario",
+                message: error.message // Use error.message para mostrar a mensagem de erro
+            });
+        }
     }
-
 
     async delete(request: Request, response: Response) {
         const { id, email } = request.body;
         try {
-            const user = await User.findOne(email);
-            if (user!) {
+            const user = await User.findOne({ email });
+            if (!user) {
                 return response.status(400).json({
                     error: "User does not exist"
                 });
+            } else {
+                await User.deleteOne({ id });
+
+                return response.json({
+                    message: "User deleted successfully"
+                });
             }
-            else {
-            await User.deleteOne({ id }); 
-    
-            return response.json({
-                message: "User deleted successfully"
-            });
-        }
-        } catch (error) {
+        } catch (error: any) {
             return response.status(500).json({
                 error: "Deletion error",
-                message: error
+                message: error.message // Use error.message para mostrar a mensagem de erro
             });
         }
     }
-    
-    
+
+    async login(request: Request, response: Response) {
+        const { email, password } = request.body;
+
+        try {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                return response.status(400).json({
+                    error: "oops",
+                    message: "User does not exist",
+                });
+            }
+
+            const passwordMatch = await bcrypt.compare(password, user.password);
+
+            if (!passwordMatch) {
+                return response.status(400).json({
+                    error: "oops",
+                    message: "Incorrect password",
+                });
+            }
+
+            // Generate a JWT token
+            const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET!, {
+                expiresIn: process.env.JWT_EXPIRES_IN,
+            });
+
+            console.log("User password:", user.password);
+            console.log("Input password:", password);
+
+            return response.json({
+                message: "Login successful",
+                token,
+            });
+
+        } catch (error: any) {
+            console.error("Login error:", error);  // Adicione este log para depuração
+            return response.status(500).json({
+                error: "Nao foi possivel fazer login",
+                message: error.message // Use error.message para mostrar a mensagem de erro
+            });
+        }
+    }
 }
 
 
-export default new UseController();
+export default new UserController();
