3af91725dabfe3e2eeceda662de39d09305ec1e7
Thurtux <arthursantoslopes91@gmail.com>
2024-06-10 09:23:38 -0300

    Addd new route

--- src/routes.ts
+++ src/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from "express";
 import UserController from "./controllers/UserController"
 import CarController from "./controllers/CarController";
-import router from "./auth/UserRegister";
 import cors from 'cors';
 
 
@@ -11,21 +10,20 @@ routes.use(cors());
 //users routes
 routes.get("/users", UserController.find);
 
-
 routes.delete("/user", UserController.delete);
 routes.post("/user", UserController.create);
-
+routes.post("/login", UserController.login);
 
 
 //cars routes
 routes.delete("/car", CarController.delete);
 routes.get("/cars", CarController.find);
 routes.post("/car", CarController.create);
-routes.put("/car", CarController.update)
+routes.put("/car", CarController.update);
+
 export default routes;
 
 
-routes.use('/api/auth', router);
 
 
 
